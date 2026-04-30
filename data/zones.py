e1629cccd10920314d5fbe006bbccb470ee23f02
iNakzu <ignacio.antiguay@hotmail.com>
2026-04-29 21:31:12 -0400

    docker compose yml & data assets add

---
+++ data/zones.py
@@ -0,0 +1,7 @@
+ZONES = {
+    "Z1": {"name": "Providencia", "area_km2": 9.2},
+    "Z2": {"name": "Las Condes", "area_km2": 15.0},
+    "Z3": {"name": "Maipu", "area_km2": 18.5},
+    "Z4": {"name": "Santiago Centro", "area_km2": 10.8},
+    "Z5": {"name": "Pudahuel", "area_km2": 16.4},
+}
